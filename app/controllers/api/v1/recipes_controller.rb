4b1a2fe79086ab9053c46df2e8f13894e396ceef
knt930 <sword11wg@gmail.com>
2018-11-18 18:32:25 +0900

    recipesコントローラーのshowアクションの処理をrecipeの中にingredientsとinstructionsがくるように変更

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -4,24 +4,20 @@ class Api::V1::RecipesController < ApplicationController
 
   def index
     if Time.now.beginning_of_day < params[:first_launched_at]
-      render json: {recipes: @recipes.order(boosted: 'DESC')}
+      render json: {recipes: @recipes.order(boosted: :desc)}
     else
       render json: {recipes: @recipes}
     end
   end
 
   def show
-    render json: {
-        recipe: @recipe,
-        ingredients: @recipe.ingredients,
-        instruction: @recipe.instructions
-    }
+    render json: {recipe: @recipe}, include: [:ingredients, :instructions]
   end
 
   private
 
   def set_recipes
-    @recipes = Recipe.all
+    @recipes = Recipe.page(params[:page]).per(10)
   end
 
   def set_recipe
