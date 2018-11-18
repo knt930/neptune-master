65453cd9c4d9c6bd67583bbc517f23e92f000aa7
knt930 <sword11wg@gmail.com>
2018-11-18 18:31:16 +0900

    FactoryBotでテストデータの作成

---
+++ spec/factories/ingredients.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :ingredient do
+    sort_order 1
+    name 'もやし'
+    quantity_and_unit '100g'
+  end
+end
---
+++ spec/factories/recipes.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :recipe do
+    title 'hogehoge'
+    video_url ''
+    thumbnail_url ''
+    servings ''
+    salt ''
+    calorie ''
+    introduction ''
+    boosted true
+  end
+end
