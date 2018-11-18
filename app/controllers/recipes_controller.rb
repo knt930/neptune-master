4f441c8ae6f555482a63a003c52527e56035ad05
knt930 <sword11wg@gmail.com>
2018-11-19 02:18:42 +0900

    recipes_controllerのnew, createアクションを実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,25 @@
 class RecipesController < ApplicationController
   def new
-
+    @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.instructions.build
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to root_path
+    else
+      render :new
+    end
 
   end
+
+  private
+
+    def recipe_params
+      params.require(:recipe).permit(:title, :video_url, :thumbnail_url, :servings, :salt, :calorie, :introduction,
+                                     ingredients_attributes: [:id, :sort_order, :name, :quantity_and_unit, :_destroy],
+                                      instructions_attributes: [:id, :sort_order, :body, :_destroy])
+    end
 end
