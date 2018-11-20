a20f238c84439168167ea6851fb278f70e5078bd
knt930 <sword11wg@gmail.com>
2018-11-20 15:31:59 +0900

    一つのレシピのカロリーと塩分量を算出するスクリプトを、一つのメソッドに複数の処理を記述していたところをメソッドに切り出し

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,15 @@
 class Food < ApplicationRecord
   has_many :quantity
   has_one :ingredient
+
+  def return_calories(gram)
+    calorie = calories_per_100.to_f * (gram / 100)
+    calorie
+  end
+
+  def return_salts(gram)
+    salt = salts_per_100.to_f * (gram / 100)
+    salt
+  end
+
 end
